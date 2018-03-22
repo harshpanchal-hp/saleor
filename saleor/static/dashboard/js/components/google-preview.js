3bb7d8fc48ffc480e13ef68c80b39fe7b9082026
Pacu2 <filip.m.kucharczyk@gmail.com>
2018-03-22 12:37:24 +0100

    WIP errors box

--- saleor/static/dashboard/js/components/google-preview.js
+++ saleor/static/dashboard/js/components/google-preview.js
@@ -15,14 +15,40 @@ if ($descriptionMaterialize) {
 const $googleTitlePreview = $('#google-preview-title');
 const $googleDescriptionPreview = $('#google-preview-description');
 const $preview = $('.preview');
-const $previewErrors = $('.preview-error');
+const $previewErrors = $('#preview-error');
 
-const watchedEvents = 'input propertychange cut paste copy change';
+const watchedEvents = 'input propertychange cut paste copy change delete';
+
+function checkForErrors() {
+  const $descriptionText = $googleDescriptionPreview.text() || $googleDescriptionPreview.val();
+  const $titleText = $googleTitlePreview.text();
+  if ($descriptionText && $titleText) {
+    $preview.show();
+    $previewErrors.hide();
+  } else if (!$descriptionText && !$titleText) {
+    $preview.hide();
+    $previewErrors.text(gettext('title and description missing'));
+    $previewErrors.show();
+
+    // title and descriptions missing
+  } else if (!$descriptionText) {
+    $preview.hide();
+    $previewErrors.show();
+    $previewErrors.text(gettext('description missing'));
+
+    // description missing
+  } else if (!$titleText) {
+    // title missing
+    $preview.hide();
+    $previewErrors.show();
+    $previewErrors.text(gettext('title missing'));
+  }
+}
 
 function truncate(text, seoField) {
   const $fieldMaxLength = seoField.prop('maxLength');
   if ($fieldMaxLength === -1) {
-    console.log('Field maxlength is not defined');
+    // console.log('Field maxlength is not defined');
     return text;
   }
   return text.substring(text, $fieldMaxLength);
@@ -31,35 +57,34 @@ function truncate(text, seoField) {
 function updatePlaceholderOnInput(field, seoField, previewField) {
   field.on(watchedEvents, (e) => {
     const $target = $(e.currentTarget);
-    const $placeholderText = $target.val() || $target.text();
+    const $placeholderText = $target.val();
+    console.log($placeholderText);
     seoField.attr('placeholder', truncate($placeholderText, seoField));
     const $seoText = seoField.val();
+    console.log($seoText);
     if (!$seoText) {
       previewField.text(truncate($placeholderText, seoField));
+    } else {
+      previewField.text(truncate($seoText, seoField));
     }
+    checkForErrors();
   });
 }
 
 function updatePreviewOnInput(seoField, previewField) {
   seoField.on(watchedEvents, (e) => {
-    $preview.show();
-    $previewErrors.hide();
     const $target = $(e.currentTarget);
     const $currentText = $target.val();
     if ($currentText) {
       previewField.text(truncate($currentText, seoField));
     } else {
       const $placeholderValue = seoField.attr('placeholder');
-      if ($placeholderValue) {
-        previewField.text(truncate($placeholderValue, seoField));
-      } else {
-        $preview.hide();
-        $previewErrors.show();
-      }
+      previewField.text(truncate($placeholderValue, seoField));
     }
+    checkForErrors();
   });
 }
-
+checkForErrors();
 updatePlaceholderOnInput($name, $seoTitle, $googleTitlePreview);
 updatePlaceholderOnInput($description, $seoDescription, $googleDescriptionPreview);
 updatePreviewOnInput($seoTitle, $googleTitlePreview);
